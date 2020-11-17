a7b5a3423500e9e7c4fe16c25976b56861c916cb
Jocelle Elvambuena <71588038+jocelle23@users.noreply.github.com>
2020-11-16 19:20:37 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# api-capstone-project
+
+Why spend an hour or two watching something you may have had no real interest in? 
+
+I wanted to help users find a great movie! Users have the 
+ability to look up movie ratings using omdb's API. Trailers 
+related to the search will also be displayed to help the 
+user make their final decision on a movie.
+
+[API Capstone Project](https://jocelle23.github.io/api-capstone-project)
---
+++ index.js
@@ -0,0 +1,201 @@
+'use strict';
+
+const omdbApiKey = '310a9b9d';
+const omdbSearchUrl = 'https://www.omdbapi.com/';
+const youTubeApiKey = 'AIzaSyB3PboYRtBgCv8f7nRIdGrcENwunztc3nE'; //AIzaSyC6nCJGAQxIhNC12yb_h8Z74Qf3eZRsMn0
+const youTubeSearchUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+/* Functions for omdb API */
+function displayRatings(responseJson) {
+    let imdbMovieRating = responseJson.Ratings[0].Value;
+    let rtMovieRating = responseJson.Ratings[1].Value;
+    let metaMovieRating = responseJson.Ratings[2].Value;
+
+    $('#movie-ratings').html(`
+    <section class="ratings">    
+        <p><b>IMDB Rating: </b>${imdbMovieRating}</p>
+        <p><b>Rotten Tomatoes: </b>${rtMovieRating}</p>
+        <p><b>Metacritic: </b>${metaMovieRating}</p>
+    </section>
+    `
+    );
+}
+
+function displayDescription(responseJson) {
+    let moviePlot = responseJson.Plot;
+    let movieYear = responseJson.Year;
+    let movieGenre = responseJson.Genre;
+    let movieActors = responseJson.Actors;
+    let movieDirector = responseJson.Director;
+    let movieAwards = responseJson.Awards;
+
+    $('#movie-list').append(`
+    <section class="mv-description">    
+        <p><b>Director: </b>${movieDirector}</p>    
+        <p><b>Year: </b>${movieYear}</p>
+        <p><b>Genre(s): </b>${movieGenre}</p>
+        <p><b>Actors: </b>${movieActors}</p>
+    </section>
+    `
+    );
+}
+
+function displayTitleAndImage(responseJson) {
+    let movieTitle = responseJson.Title;
+    let imdbImage = responseJson.Poster;
+
+    $('#movie-title-and-img').append(`
+    <h2 class="mv-title">${movieTitle}</h2>
+    <img src="${imdbImage}" alt="Movie Image" class="movie-image">
+    `
+    );
+}
+
+function displayMovieResults(responseJson) {
+    if (responseJson.Error) {
+        $('#movie-results').addClass("hidden");
+        $('.column3').addClass("hidden");
+        $('#js-error-message').empty();
+        $('#js-error-message').removeClass("hidden");
+            $('#js-error-message').html(`  
+            <p>Sorry, ratings are unavailable for this movie.</p>
+            <br>
+            <p>Please try another movie!</p>
+            `);
+    }
+    else {
+        $('#movie-results').removeClass('hidden');
+        console.log(responseJson);
+        $('#movie-title-and-img').empty();
+        $('#movie-list').empty();
+        displayRatings(responseJson);
+        displayTitleAndImage(responseJson);
+        displayDescription(responseJson);
+    }
+}
+
+function formatOmdbQuery(omdbParams) {
+    const queryItems = Object.keys(omdbParams)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(omdbParams[key])}`)
+    return queryItems.join('&');
+}
+
+function findMovies(searchedMovie) {
+    const omdbParams = {
+        apiKey: omdbApiKey,
+        t: searchedMovie
+    };
+
+    const omdbQuerySearch = formatOmdbQuery(omdbParams);
+    const omdbUrl = omdbSearchUrl + "?" + omdbQuerySearch;
+
+    const options = {
+        headers: new Headers({
+            Referer: omdbSearchUrl,
+        })
+    };
+
+    fetch(omdbUrl, options)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayMovieResults(responseJson))
+        .catch(err => {
+            $('#movie-results').addClass("hidden");
+            console.log(err);
+            $('#js-error-message').empty();
+            $('#js-error-message').removeClass("hidden");
+            $('#js-error-message').html(`  
+            <p>Sorry, ratings are unavailable for this movie.</p>
+            <br>
+            <p>Please try another movie!</p>
+            `);
+
+        })
+}
+
+/* Functions for YouTube API */
+function displayTrailerResults(responseJson2) {
+    $('#trailer-results').removeClass('hidden');
+    console.log(responseJson2);
+    $('#trailer-list').empty();
+
+    for (let i = 0; i < responseJson2.items.length; i++) {
+        let videoTitle = responseJson2.items[i].snippet.title;
+        let setLength = 35;
+        let choppedVideoTitle = videoTitle.substring(0, setLength);
+        let videoId = responseJson2.items[i].id.videoId;
+
+        $('#trailer-list').append(`
+        <p class="trailer-titles"><b>${choppedVideoTitle}</b></p>
+        <iframe width="250" height="175" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        `
+        )
+    };
+}
+
+function formatYouTubeQuery(youTubeParams) {
+    const queryItems = Object.keys(youTubeParams)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(youTubeParams[key])}`)
+    return queryItems.join('&');
+}
+
+function findTrailers(searchedMovie) {
+    let movieSearch = searchedMovie + "trailer";
+    const youTubeParams = {
+        part: 'snippet',
+        maxResults: 2,
+        q: movieSearch,
+        key: youTubeApiKey
+    };
+
+    const youTubeQuerySearch = formatYouTubeQuery(youTubeParams);
+    const youTubeUrl = youTubeSearchUrl + "?" + youTubeQuerySearch;
+
+    fetch(youTubeUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson2 => displayTrailerResults(responseJson2))
+        .catch(err => {
+            $('#js-error-message').text(`Error: ${err.message}`);
+            console.log(err);
+            $('#movie-results').addClass("hidden");
+            $('.column3').addClass("hidden");
+        })
+}
+
+// Transition from main page to column format
+function homeToMainPage() {
+    $('#main-title').removeClass("welcome-page");
+    $('.home-bckgrnd-image').addClass("hidden");
+    $('#main-title').addClass("column1");
+    $('#ww-title').removeClass("no-link-style");
+    $('#ww-title').addClass("black-link-style");
+    $('#p-1').removeClass("main-p");
+    $('#p-1').addClass("main-b-p");
+    $('#p-2').removeClass("main-p1");
+    $('#p-2').addClass("main-b-p1");
+}
+
+function watchForm() {
+    $('#search-form').submit(event => {
+        event.preventDefault();
+        $('#js-error-message').empty();
+        $('#js-error-message').addClass("hidden");
+        homeToMainPage();
+        // Declare user's input as a variable
+        const searchedMovie = $("#search-bar").val();
+        console.log("Looking for related movies...");
+        findMovies(searchedMovie);
+        findTrailers(searchedMovie);
+    });
+}
+
+$(watchForm);
